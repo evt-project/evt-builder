67d7462d9c69cc899d22b640d7160fe16cc3287c
Chiara Di Pietro <dipi.chiara@gmail.com>
2013-12-06 14:04:14 +0100

    Add animation full screen

--- js/main/interface_control.js
+++ js/main/interface_control.js
@@ -563,5 +563,31 @@ $( function() {
 			..codice qui..
 		});*/	
 	/* / Gestione click */
+	$(document).keydown(function(e){	
+			if (e.keyCode == 70){
+				$('#main_left_frame').toggleClass('full');
+				$('#left_header').toggle('blind');				
+				if($('#main_left_frame').hasClass('full')){
+					var height_full = ($(window).height()>$("body").height()) ? $(window).height() : $("body").height();
+					var width_full = $(window).width();
+					var margin_left = -($('#main_left_frame').offset().left);
+					$('#main_left_frame').animate({
+						width: width_full,
+						height: height_full,
+						top: "-71px",
+						left: margin_left,
+						minWidth: "1021px"
+					}, 700);
+				} else {
+					$('#main_left_frame').animate({
+						width: "49.8%",
+						height: "100%",
+						top: "0px",
+						left: "0px",
+						minWidth: "0px"
+					}, 700);
+				}
+			}
+		});
 
 });
