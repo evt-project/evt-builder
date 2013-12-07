0cdc1b0d64e850f43a8ad0d9a12073dcaa4d249d
Chiara Di Pietro <dipi.chiara@gmail.com>
2013-12-07 13:48:55 +0100

    Fix fullScreen on resize / Changes on left header menu buttons

--- js/main/fullscreen_request.js
+++ js/main/fullscreen_request.js
@@ -103,10 +103,11 @@ function fullScreenLeft(){
 		if (!$("#main_right_frame").hasClass('full')){
 				$("#main_left_frame").toggleClass("full");
 				if($('#main_left_frame').hasClass('full')){
-					var height_full = ($(window).height()>$("body").height()) ? $(window).height()-4 : $("body").height();
-					var width_full = $(window).width()-4; /* "-4" perché il box ha bordo 2px */
+					var height_full = ($(window).height() > $("body").height()) ? $(window).height()-4 : $("body").height();
+					var width_full = ($(window).width()>$("#global_wrapper").width()) ? $(window).width()-4 : $("#global_wrapper").width();
 					var margin_left = -($('#main_left_frame').offset().left);
 					var margin_top = -($('#main_left_frame').offset().top);
+					$('.go-full-right').css("z-index", "0");
 					$('#main_left_frame').animate({
 						width: width_full,
 						height: height_full,
@@ -114,7 +115,21 @@ function fullScreenLeft(){
 						left: margin_left,
 						minWidth: "1021px"
 					}, 700);
+
+					var leftR = -($('.go-full-left').offset().left+11);
+					$('.go-full-left').animate({
+						"left": leftR,
+    					"color": "#F5EAD4",
+    					"top": "-76px",
+					}, 700);
+					$('.go-full-left').toggleClass('closeFullScreen');
 				} else {
+					$('.go-full-right').css("z-index", "99999999");
+					$('.go-full-left').animate({
+						"left": "-3px",
+						"color": "#4E443C",
+						"top": "-11px"
+					}, 700);
 					if($('#main_right_frame').css("display")=="none"){
 						$('#main_left_frame').animate({
 							width: "99.5%", 
@@ -132,38 +147,53 @@ function fullScreenLeft(){
 							minWidth: "0px"
 						}, 700);
 					}
+					$('.go-full-left').toggleClass('closeFullScreen');
 				}
-				$(".top_image_tools").toggle("bind");
-				$('.go-full-left').toggleClass('closeFullScreen', 700);
 			}
 	}
 
 	function fullScreenRight(){
-			if (!$("#main_left_frame").hasClass('full')){
-				$("#main_right_frame").toggleClass("full");
-				if($('#main_right_frame').hasClass('full')){
-					var height_full = ($(window).height()>$("body").height()) ? $(window).height()-4 : $("body").height();
-					var width_full = $(window).width()-4; /* "-4" perché il box ha bordo 2px */
-					var margin_left = -($('#main_right_frame').offset().left);
-					var margin_top = -($('#main_right_frame').offset().top);
-					$('#main_right_frame').animate({
-						width: width_full,
-						height: height_full,
-						marginTop: margin_top,
-						left: margin_left,
-						minWidth: "1021px"
-					}, 700);
-				} else {
-					$('#main_right_frame').animate({
-						width:  "49.7%",
-						height: "100%", 
-						marginTop: "0px", 
-						left: "0px", 
-						minWidth: "0px"
-					}, 700, function(){
-						$('#main_right_frame').removeAttr("style");
-					});
-				}
-				$('.go-full-right').toggleClass('closeFullScreen', 700);
-			}
+		$("#main_right_frame").toggleClass("full");
+		if($('#main_right_frame').hasClass('full')){
+			var height_full = ($(window).height() > $("body").height()) ? $(window).height()-4 : $("body").height();
+			var width_full = ($(window).width()>$("#global_wrapper").width()) ? $(window).width()-4 : $("#global_wrapper").width();
+			var margin_left = -($('#main_right_frame').offset().left);
+			var margin_top = -($('#main_right_frame').offset().top);
+			$('.go-full-left').css("z-index", "0");
+			
+			$('#main_right_frame').animate({
+				width: width_full,
+				height: height_full,
+				marginTop: margin_top,
+				left: margin_left,
+				minWidth: "1021px"
+			}, 700);
+			
+			var rightR = -((width_full-$('.go-full-right').offset().left)-20);
+			$('.go-full-right').animate({
+				"right": rightR,
+				"top": "-8px",
+			    "color": "#F5EAD4"
+			},700);
+
+			$('.go-full-right').toggleClass('closeFullScreen');
+			
+		} else {
+			$('.go-full-left').css("z-index", "999999999999");
+			$('.go-full-right').animate({
+				"right": "-4px",
+				"color": "#4E443C",
+				"top": "-11px"
+			}, 700);
+			$('#main_right_frame').animate({
+				width:  "49.7%",
+				height: "100%", 
+				marginTop: "0px", 
+				left: "0px", 
+				minWidth: "0px"
+			}, 700, function(){
+				$('#main_right_frame').removeAttr("style");
+			});
+			$('.go-full-right').toggleClass('closeFullScreen');
+		}
 	}
