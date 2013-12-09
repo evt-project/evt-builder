dfe8e9dad336ec2c1223c22943d6c2fa0557398f
ChiaraDipi <dipi.chiara@gmail.com>
2013-12-09 15:33:31 +0100

    Fix resize div/select - minor changes - add alternative background images

--- js/main/fullscreen_request.js
+++ js/main/fullscreen_request.js
@@ -194,6 +194,7 @@ function closeFullScreenLeft(){
 function goFullScreenRight(){
 	if($('#header_collapse').hasClass('fa-caret-down')) collapseHeader();
 	$('#header_collapse').toggle();
+	// Gestione del full screen per browser webkit
 	if ($.browser.webkit) {
 		var height_full = ($(window).height() > $("body").height()) ? $(window).height()-4 : $("body").height();
 		var width_full = ($(window).width()>$("#global_wrapper").width()) ? $(window).width()-4 : $("#global_wrapper").width()-4;
@@ -237,6 +238,7 @@ function goFullScreenRight(){
 } 
 
 function closeFullScreenRight(){
+	// Gestione del full screen per browser webkit
 	if ($.browser.webkit) {
 		var widthLeft = $('#main_left_frame').width()-4;
 		$('#main_right_frame').animate({
