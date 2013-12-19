027538aa009b509dc5dd4c0f81e93a6d6d369487
Chiara Di Pietro <dipi.chiara@gmail.com>
2013-12-19 01:50:27 +0100

    Header layout changes and problem with collapse menu solved

--- js/main/interface_control.js
+++ js/main/interface_control.js
@@ -438,6 +438,189 @@ $( function() {
 		}
 	}
 
+	function goFullScreenLeft(){
+		if (ITLon){UnInitialize();}
+		$('.zoomWindow').hide();
+        $('.zoomPup').hide();
+		if($('#header_collapse').hasClass('fa-caret-down')) 
+			$("#header_collapse").trigger('click');
+		$('#header_collapse').toggle();
+		$("#main_left_frame").toggleClass("full");
+		var height_full = ($(window).height() > $("body").height()) ? $(window).height()-4 : $("body").height();
+		var width_full = ($(window).width()>$("#global_wrapper").width()) ? $(window).width()-4 : $("#global_wrapper").width()-4;
+		// 4 è l'altezza totale dei bordi di sinistra e destra
+		var margin_left = -($('#main_left_frame').offset().left);
+		var margin_top = -($('#main_left_frame').offset().top);
+		
+		//cambia dimensione elementi per Mag //Add for Mag
+		left_headerHeight = $('#left_header').height();
+        $('#mag_image_elem').css({'margin-top': left_headerHeight+'px', 'height': height_full-left_headerHeight+'px'});
+        $('.zoomWindow').css({left: (width_full - $(".zoomWindow").width())/2+'px'});
+        $('.zoomPup').css({left: (width_full - $(".zoomPup").width())/2+'px'});
+        
+		$('#main_left_frame').animate({
+			width: width_full,
+			height: height_full,
+			top: margin_top,
+			left: margin_left,
+			minWidth: "1021px"
+		}, 700, function(){
+			$('#left_header .closeFullScreen').toggle();
+			$('.zoomWindow').show(0); //Add for mag
+			//$('#header_collapse').animate({opacity: 1});
+		});
+		$('.go-full-left').toggle();
+		$('#switchITL:visible').hide();
+		
+		if(($('#span_ee_select-add').is('visible'))&&!$('#span_ee_select-add').hasClass('widthChanged')){
+			$('#span_ee_select-add').addClass('widthChanged')
+			$('#span_ee_select-add .option_container').removeAttr('style');
+
+			$('#span_ee_select-add').each(function(){
+				var widthSel = $(this).width();
+				var widthOpt = $(this).find('.option_container').width()+10;
+				if(widthSel > (widthOpt+24)){ 
+					$(this).find('.option_container').css('width', widthSel-10);
+					//alert("widthSel"); 
+				} else {
+					$(this).css('width', widthOpt+24);
+					$(this).find('.option_container').css('width', widthOpt+14);
+					//alert("widthOpt");
+				}
+			});
+		}
+	}
+
+	function closeFullScreenLeft(){
+		//$('#header_collapse').animate({opacity: 0});
+		$("#main_left_frame").toggleClass("full");
+		$('.zoomWindow').hide();
+	    $('.zoomPup').hide();
+		//caso in cui si passa a fullscreen dalla visualizzazione a doppia pagina
+		if($('#main_right_frame').css("display")=="none"){
+			$('#main_left_frame').animate({
+				width: "99.5%", 
+				height: "100%", 
+				top: "0px", 
+				left: "0px", 
+				minWidth: "0px"
+			}, 700, function(){
+				$('#left_header .closeFullScreen').toggle();
+				$('.go-full-left').toggle();
+				//$('#header_collapse').removeAttr('style');
+				$('#header_collapse').toggle();
+				//Add for Mag
+				setMagHeight();
+	            $('.zoomWindow').show();
+			});
+		} else {
+			$('#main_left_frame').animate({
+				width: "49.8%",
+				height: "100%",
+				top: "0px",
+				left: "0px",
+				minWidth: "0px"
+			}, 700, function(){
+				$('#left_header .closeFullScreen').toggle();
+				$('.go-full-left').toggle();
+				//$('#header_collapse').removeAttr('style');
+				$('#header_collapse').toggle();
+				//Add for Mag
+				setMagHeight();
+	            $('.zoomWindow').show();
+	            checkAnnPosHS(); //Add for HS
+			});
+			$('#switchITL').show();
+		}
+	    
+		$('#span_dd_select').hide();
+	}
+
+	function goFullScreenRight(){
+	    if (ITLon){UnInitialize();} //Add by JK for ITL
+	    if (HSon){UnInitialize();} //Add by JK for HS
+		if($('#header_collapse').hasClass('fa-caret-down')) 
+			$("#header_collapse").trigger('click');
+		$('#header_collapse').toggle();
+		// Gestione del full screen per browser webkit
+		if ($.browser.webkit) {
+			var height_full = ($(window).height() > $("body").height()) ? $(window).height()-4 : $("body").height();
+			var width_full = ($(window).width()>$("#global_wrapper").width()) ? $(window).width()-4 : $("#global_wrapper").width()-4;
+			// 4 è l'altezza totale dei bordi di sinistra e destra
+			var margin_right = -($(window).width()-($('#main_right_frame').offset().left+$('#main_right_frame').width())-4);
+			var margin_top = -($('#main_right_frame').offset().top);
+			$("#main_right_frame").toggleClass("full");
+			$('#main_right_frame').animate({
+				width: width_full,
+				height: height_full,
+				marginTop: margin_top,
+				right:margin_right,
+				minWidth: "1021px"
+			}, 700, function(){
+				$('#right_header .closeFullScreen').toggle();
+			});
+			$("#main_right_frame").css('position', 'absolute');
+			$('.go-full-right').toggle();
+			
+		} else {
+			var height_full = ($(window).height() > $("body").height()) ? $(window).height()-4 : $("body").height();
+			var width_full = ($(window).width()>$("#global_wrapper").width()) ? $(window).width()-4 : $("#global_wrapper").width()-4;
+			// 4 è l'altezza totale dei bordi di sinistra e destra
+			var margin_right = -($(window).width()-($('#main_right_frame').offset().left+$('#main_right_frame').width())-4);
+			var margin_top = -($('#main_right_frame').offset().top);
+			var margin_left = -($('#main_right_frame').offset().left);
+			$("#main_right_frame").toggleClass("full");
+
+			$('#main_right_frame').animate({
+				width: width_full,
+				height: height_full,
+				marginTop: margin_top,
+				left: margin_left,
+				right:margin_right,
+				minWidth: "1021px"
+			}, 700, function(){
+				$('#right_header .closeFullScreen').toggle();
+			});
+			
+			$('.go-full-right').toggle();
+		}
+	} 
+
+	function closeFullScreenRight(){
+	    if ($('#switchITL i ').hasClass('fa-chain')){Initialize();} //Add by JK for ITL
+	    if ($('#switchHS i ').hasClass('fa fa-dot-circle-o')){InitializeHS();} //Add by JK for HS
+	    // Gestione del full screen per browser webkit
+		if ($.browser.webkit) {
+			var widthLeft = $('#main_left_frame').width()-4;
+			$('#main_right_frame').animate({
+				width: widthLeft,
+				height: "100%", 
+				marginTop: "0px", 
+				right: "0px", 
+				minWidth: "0px",
+			}, 700, function(){
+				$('#main_right_frame').removeAttr("style");
+				$('#right_header .closeFullScreen, #header_collapse').toggle();
+				$('#main_right_frame').toggleClass("full");
+				$('.go-full-right').toggle();
+			});
+		} else {
+			var widthLeft = $('#main_left_frame').width()-3;
+			$('#main_right_frame').animate({
+				width: widthLeft,
+				height: "100%", 
+				marginTop: "0px", 
+				left: "0px", 
+				minWidth: "0px",
+			}, 700, function(){
+				$('#main_right_frame').removeAttr("style");
+				$('#right_header .closeFullScreen,  #header_collapse').toggle();
+				$('#main_right_frame').toggleClass("full");
+				$('.go-full-right').toggle();
+			});
+		}
+	}
+
 	/* / Funzioni */
 	
 	
@@ -540,7 +723,7 @@ $( function() {
 			$("#span_ee_select-add").hide();
 
 			$("#span_dd_select").hide();
-
+			
 			$("#main_right_frame").show();
 			$("#main_left_frame").animate({
 	            'width': '49.8%' 
@@ -553,7 +736,8 @@ $( function() {
 			$("#image_menu").show();
 			$('#switchITL').show();
 			$("#image_cont").show();
-			
+			if($('#right_header').hasClass('menuClosed'))
+				$('#right_header').hide();
 			
 			$('#thumb_elem').show();
 			$('#zvalint').show();
@@ -613,6 +797,8 @@ $( function() {
 			
 
 			$("#main_right_frame").show();
+			if($('#right_header').hasClass('menuClosed'))
+				$('#right_header').hide();
 			$("#main_left_frame").animate({
 	            'width': '49.8%'
 	        }, function(){
@@ -695,7 +881,7 @@ $( function() {
 			$("#image_menu").show();
 			$("#image_cont").show();
 			$('#span_dd_select').css({display: "inline-block"});
-			$('#switchITL:visible').hide();
+			$('#switchITL').hide();
 			$('#thumb_elem').hide();
 		}
 		if(!$('#span_dd_select').hasClass('widthChanged')){
@@ -715,28 +901,36 @@ $( function() {
 	$("#header_collapse").click(function(){	
 		if (magnifierON==false) 
 			$('#image_tool').slideToggle().toggleClass('menuClosed');
-
-		$('#left_header').toggle('blind').toggleClass('menuClosed');
+		// Modifico colore dell'icona .go-full-right che altrimenti non si vedrebbe
 		$('.go-full-right').toggleClass('onWhite');
+		
+		// Gestione menu sinistro: chiudo/apro + aggiungo/tolgo la classe menuClosed
+		$('#left_header').toggle('blind').toggleClass('menuClosed');
+		// Se è aperto il text di sinistra modifico colore dell'icona go-full-left che altrimenti non si vedrebbe
 		if($('#text_cont-add').css('display')=='block'){
 			$('.go-full-left').toggleClass('onWhite');
 		}
 		setMagHeight(); //Add for Mag
+		// se ho appena chiuso il menu di sinistra
 		if($('#left_header').hasClass('menuClosed')){
+			// Modifico le dimensioni del testo di sinistra per riempire il box
 			noMenu_height = $('#image_cont').height();
-			$('#image_cont, #text_cont-add').animate({
-				//top: "-42px",
+			$('#text_cont-add').animate({
+				top: "-42px",
 				height: noMenu_height
 			});
 		} else {
-			//noMenu_height = $('#image_cont').height()+42;
-			$('#image_cont, #text_cont-add').animate({
-				//top: "0px",
+			// altrimenti (se ho appena aperto il menu sx)
+			// Risistemo il box del testo a sinistra
+			$('#text_cont-add').animate({
+				top: "0px",
 				height: "100%"
 			});
 		}
 
+		// Gestione menu destro: chiudo/apro + aggiungo/tolgo la classe menuClosed
 		$('#right_header').toggle('blind').toggleClass('menuClosed');
+		// Se ho appena chiuso il menu di destra
 		if($('#right_header').hasClass('menuClosed')){
 			$('#text_cont, #text_cont-add').animate({
 				height: "100%"
@@ -744,10 +938,11 @@ $( function() {
 		} else {
 			noMenu_height = $('#image_cont').height()+42;
 			$('#text_cont, #text_cont-add').animate({
-				height: "93.4%"
+				height: "calc(100% - 42px)"
 			});
 		}
 
+		// Modifico lo stile e la posizione dell'icona
 		if($('#header_collapse').hasClass('fa-caret-up')){
 			if($(".closeFullScreen:visible").length>0){
 				$('#header_collapse').animate({
@@ -778,7 +973,19 @@ $( function() {
 		}
 		
 	});	
-
+	
+	$("#goFullScreenLeft").click(function(){
+		goFullScreenLeft();
+	});
+	$("#goFullScreenRight").click(function(){
+		goFullScreenRight();
+	});
+	$("#closeFullScreenLeft").click(function(){
+		closeFullScreenLeft();
+	});
+	$("#closeFullScreenRight").click(function(){
+		closeFullScreenRight();
+	});
 	/* / Gestione click */
 	
 	/*$("#text_copy").live("click", function(){
