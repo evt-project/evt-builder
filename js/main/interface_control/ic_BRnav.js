08309f5a56035cfd2582f61118dd8bbee3be0d2a
Chiara Di Pietro <dipi.chiara@gmail.com>
2017-07-27 11:58:11 +0200

    Modified elements added in arrayPages. Adapted slider's function.

--- js/main/interface_control/ic_BRnav.js
+++ js/main/interface_control/ic_BRnav.js
@@ -1,90 +1,66 @@
 // Funzione al click su ImgTxt
 function createSliderTxtImg() {
-	var arrayPages = [];  // Array che conterrà il numero delle pagine
-	if($('.main_pp_select')) {
+	var arrayPages = []; // Array che conterrà il numero delle pagine
+	if ($('.main_pp_select')) {
 		$('.main_pp_select .option_container .optionGroup div').each(function() {
-			var val_page = $(this).attr('data-value');
-			//alert(val_page);
-			//var val_page = $(this).text();
-			arrayPages.push(val_page); // Lo mette nell'array
+			var pageId = $(this).attr('data-value');
+			var pageLabel = $(this).text();
+			var docId = $(this).attr('data-first-doc');
+			arrayPages.push({
+				id: pageId,
+				label: pageLabel,
+				firstDoc: docId
+			}); // Lo mette nell'array
 		});
-	createBrnav(arrayPages);  // Restituisce l'array contenente le pagine
+		createBrnav(arrayPages); // Restituisce l'array contenente le pagine
 	}
 }
 
+// TODO: create function fo click on TxtTxt
+function createSliderTxtTxt() {
+
+} 
+
 // Funzione al click su Bookreader
 function createSliderBookreader() {
-	var val_pages;
 	var arrayPagePairs = [];
-	if($('.main_dd_select')) { 
+	if ($('.main_dd_select')) {
 		$('.main_dd_select .option_container .option').each(function() {
-			val_pages = $(this).attr('data-value');
-			//val_pages = $(this).text();
-			//alert(val_pages);
-			arrayPagePairs.push(val_pages);
+			var pageId = $(this).attr('data-value');
+			var pageLabel = $(this).text()
+			var docId = $(this).attr('data-first-page-first-doc');
+			arrayPagePairs.push({
+				id: pageId,
+				label: pageLabel,
+				firstDoc: docId
+			});
 		});
-	createBrnav(arrayPagePairs);
+		createBrnav(arrayPagePairs);
 	}
 }
 
 // Funzione per creare lo slider (gli viene passato l'array a seconda che è stata cliccata un tipo di modalità)
 function createBrnav(array) {
 	//alert(array);
-	$("#pagenum").text(array[0] + "/" + array[array.length-1]); // Mostra il numero della pagina sul totale della pagine
+	var pageNum = array && array.length > 0 ? array[0].label + "/" + array[array.length - 1].label : '';
+	$("#pagenum").text(pageNum); // Mostra il numero della pagina sul totale della pagine
 	$('#BRpager').slider({
-        min: 0,
-        max: array.length-1,
+		min: 0,
+		max: array.length - 1,
 		/*slide: function(event, ui) {  // E' lanciato durante il trascinamento del cursore
 			$("#pagenum").text(array[ui.value] + "/" + array[array.length-1]);
 		},*/
 		change: function(event, ui) {
-			//alert(ui.value);
-			if ( !$("#imgd_link").hasClass("current_mode")) {
-				if (groupingPagesByDoc) {
-					var current_opt = $('.main_pp_select .option_container .option.selected:first');
-					var current_pp = current_opt.attr("data-value"); // Pagina corrente					
-					var newPage = array[ui.value]; // Elemento dell'array di indice ui.value (sarà quello su cui si sposta lo slider)
-					//alert(newPage);
-					
-					if(current_opt.siblings().hasClass('option')) {
-						var new_pp_opt = $("div[data-value='"+newPage+"'"+"]");  // La variabile prende il div con attributo fol_+newPage (cioè la nuova pagina)
-					}
-				}
-					
-				if(new_pp_opt != null) {
-					var new_tt_val = new_pp_opt.attr('data-first-doc');
-				}
+			var newPage = array[ui.value]; // Elemento dell'array di indice ui.value (sarà quello su cui si sposta lo slider)
+			var newPageId = newPage ? newPage.id : '';
+			var newPageLabel = newPage ? newPage.label : '';
+			var newDocId = newPage ? newPage.firstDoc : '';
+			if (newPage) { // Check just to be sure
+				updateHash(newDocId, newPageId, '');
 				
-			}else{
-				$('#span_dd_select .label_selected').attr('data-last-hash-txtimg', '');
-				current_opt = $('.main_dd_select .option_container .option.selected');
-				current_pp = current_opt.attr('data-value');				
-				var newPage = array[ui.value];
-				//alert(newPage);				
-				
-				//newPage = newPage.replace(" - ", "+fol_"); // Sostituisco (ad esempio 214v - 215r con 241v+fol_215r)
-				if(current_opt.siblings().hasClass('option')) {
-					var new_pp_opt = $("div[data-value='"+newPage+"'"+"]");  // La variabile prende il div con attributo fol_+newPage (cioè la nuova pagina)
-				}
-				
-				if(new_pp_opt != null) {
-					new_tt_val = new_pp_opt.attr('data-first-page-first-doc');
-				}				
+				var pageNum = array && array.length > 0 ? newPageLabel + "/" + array[array.length - 1].label : '';
+				$("#pagenum").text(pageNum);
 			}
-			
-			if (new_pp_opt != null) {
-				new_pp_val = new_pp_opt.attr('data-value'); // Pagina risultante dallo slider
-				//alert(new_pp_val);
-        
-				var current_tt_val = $(".main_tt_select .label_selected").attr("data-value"); 
-				var current_tt_first_page = $(".main_tt_select .option_container .option.selected").attr('data-first-page');
-				if(current_tt_first_page === new_pp_val) {
-					updateHash(current_tt_val, new_pp_val, "");  // Aggiorno la hash
-				}else{
-					updateHash(new_tt_val, new_pp_val, "");
-				}
-			}
-			$("#pagenum").text(array[ui.value] + "/" + array[array.length-1]); 
 		}
 	});
 }
