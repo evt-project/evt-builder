cea028b7ebeb5f13762bad378e82a536ebeb515f
Rocco Russo <rocco.russo95@gmail.com>
2018-01-03 11:43:16 +0100

    Edited initializeProse() function

--- js/main/interface_control/ic_viewVerses.js
+++ js/main/interface_control/ic_viewVerses.js
@@ -1,6 +1,6 @@
 function initializeProse(){
-    var space=document.getElementsByClassName("spazio");
-    for (var i=0; i<space.length; i++){
-        space[i].innerHTML="<br />";
+    var lb=document.getElementsByClassName("lb");
+    for (var i=1; i<lb.length; i++){
+        lb[i].innerHTML="<br />";
     }
 }
